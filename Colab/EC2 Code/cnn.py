a4791fb94ad2196eda230b357262c9c9987e9d3c
Ubuntu <ubuntu@ip-172-31-32-155.us-west-2.compute.internal>
2022-05-13 06:34:59 +0000

    Initial EC2 code

---
+++ Colab/EC2 Code/cnn.py
@@ -0,0 +1,260 @@
+import sys
+from concurrent.futures import ThreadPoolExecutor
+import os
+import magic
+
+import numpy as np
+import pandas as pd
+import sklearn
+from tqdm.auto import tqdm
+import scipy
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader
+from torch.utils.data import sampler
+
+import torchvision.datasets as dset
+import torchvision.transforms as T
+
+import torch.nn.functional as F  # useful stateless functions
+
+# Note: when testing on your own may have to change this to reset your python path
+# import get_partial_data
+
+data_clean_dir = '../../../../231nproj/data_clean/' 
+
+def load_data(data):
+# parameter data is 'all', 'partial', or 'random'. 
+# 'all' and 'partial' loads the respective .npz stored files, 'random' randomly generates new partial data.
+    print('loading data...')
+
+    if data == 'all':
+        # load all data
+        train_data = np.load(data_clean_dir + 'train.npz', allow_pickle=True)
+        train_X, train_Y = train_data['train_X'], train_data['train_Y']
+        print("train_X: ", train_X.shape)
+        print("train_Y: ", train_Y.shape)
+
+        val_data = np.load(data_clean_dir + 'val.npz', allow_pickle=True)
+        val_X, val_Y = val_data['val_X'], val_data['val_Y']
+        print("val_X: ", val_X.shape)
+        print("val_Y: ", val_Y.shape)
+
+        trainval_X, trainval_Y = np.concatenate((train_data['train_X'], val_data['val_X']), axis=0), np.concatenate((train_data['train_Y'], val_data['val_Y']), axis=0)
+        print("trainval_X: ", trainval_X.shape)
+        print("trainval_Y: ", trainval_Y.shape)
+
+        train_data = np.load(data_clean_dir + 'test.npz', allow_pickle=True)
+        test_X, test_Y = test_data['test_X'], test_data['test_X'], 
+        print("test_X: ", test_X.shape)
+        print("test_Y: ", test_Y.shape)
+    elif data == 'partial':
+        train_data = np.load(data_clean_dir + 'train_partial.npz', allow_pickle=True)
+        train_X, train_Y = train_data['train_X'], train_data['train_Y']
+        print("train_X: ", train_X.shape)
+        print("train_Y: ", train_Y.shape)
+
+        val_data = np.load(data_clean_dir + 'val_partial.npz', allow_pickle=True)
+        val_X, val_Y = val_data['val_X'], val_data['val_Y']
+        print("val_X: ", val_X.shape)
+        print("val_Y: ", val_Y.shape)
+
+        trainval_X, trainval_Y = np.concatenate((train_data['train_X'], val_data['val_X']), axis=0), np.concatenate((train_data['train_Y'], val_data['val_Y']), axis=0)
+        print("trainval_X: ", trainval_X.shape)
+        print("trainval_Y: ", trainval_Y.shape)
+
+        test_data = np.load(data_clean_dir + 'test_partial.npz', allow_pickle=True)
+        test_X, test_Y = test_data['test_X'], test_data['test_X'], 
+        print("test_X: ", test_X.shape)
+        print("test_Y: ", test_Y.shape)
+    else: 
+        print('generating random data...')
+        label = "n_under5_mort" 
+        train_X, train_Y = get_partial_data.get_data_split(label, 'train', 0.005)
+        print("train_X: ", train_X.shape)
+        print("train_Y: ", train_Y.shape)
+
+        val_X, val_Y = get_partial_data.get_data_split(label, 'val', 0.005)
+        print("val_X: ", val_X.shape)
+        print("val_Y: ", val_Y.shape)
+
+        test_X, test_Y = get_partial_data.get_data_split(label, 'test', 0.005)
+        print("test_X: ", test_X.shape)
+        print("test_Y: ", test_Y.shape)
+    return torch.from_numpy(train_X), torch.from_numpy(train_Y), torch.from_numpy(val_X), torch.from_numpy(val_Y), torch.from_numpy(test_X), torch.from_numpy(test_Y)
+
+
+
+train_X, train_Y, val_X, val_Y, test_X, test_Y = load_data(data='partial')  #used to be random
+print('finished loading data.')
+
+
+USE_GPU = True
+dtype = torch.float32 
+
+if USE_GPU and torch.cuda.is_available():
+    device = torch.device('cuda')
+else:
+    device = torch.device('cpu')
+
+# Constant to control how frequently we print train loss.
+print_every = 100
+print('using device:', device)
+
+
+# function adapted from PyTorch.ipynb from CS 231N Assignment 2
+def check_accuracy_part34(X, Y, model, val_or_test):
+    if val_or_test == "val":
+        print('Checking accuracy on validation set')
+    elif val_or_test == "test":
+        print('Checking accuracy on test set')
+
+    batch_size = 32
+    num_batches = Y.shape[0] // batch_size   
+    num_correct = 0
+    num_samples = 0
+    model.eval()  # set model to evaluation mode
+    
+    all_preds = []
+    with torch.no_grad():
+        model = model.to(device=device)  # move the model parameters to CPU/GPU
+        for t in range(num_batches):
+          x = X[t*batch_size:(t+1)*batch_size, :, :, :]
+          y = Y[t*batch_size:(t+1)*batch_size]
+          x = x.to(device=device, dtype=dtype)  # move to device, e.g. GPU
+          y = y.to(device=device, dtype=torch.long)
+          scores = model(x).cpu().numpy()
+          #print(scores.shape)
+          preds = np.argmax(scores, axis=1)
+          num_correct += (preds == y.cpu().numpy()).sum()
+          num_samples += preds.shape[0]
+          
+          # for r^2
+          all_preds.append(preds)
+        all_preds = np.concatenate(all_preds, axis=0)
+        print('preds:', all_preds[:10], 'actual:', Y.cpu().numpy()[:10])
+        r2, _ = scipy.stats.pearsonr(all_preds, Y.cpu().numpy()[:all_preds.shape[0]])
+        r2 = r2 ** 2
+        acc = float(num_correct) / num_samples
+        print('Got %d / %d correct (%.2f)' % (num_correct, num_samples, 100 * acc), ' and an r^2 value of', r2)
+    return acc, r2
+
+
+# function adapted from PyTorch.ipynb from CS 231N Assignment 2
+def train_part34(model, optimizer, val_or_test, epochs=1):
+    """
+    Train a model using the PyTorch Module API.
+    
+    Inputs:
+    - model: A PyTorch Module giving the model to train.
+    - optimizer: An Optimizer object we will use to train the model
+    - epochs: (Optional) A Python integer giving the number of epochs to train for
+    
+    Returns: Nothing, but prints model accuracies during training.
+    """
+    
+    batch_size = 32
+    if val_or_test == "val":
+      X = train_X
+      Y = train_Y
+    elif val_or_test == "test":
+      X = trainval_X
+      Y = trainval_Y
+    num_batches = Y.shape[0] // batch_size
+
+    model = model.to(device=device)  # move the model parameters to CPU/GPU
+    for e in range(epochs):
+      # make minibatches x, y from X, Y
+        for t in range(num_batches):
+          x = X[t*num_batches:(t+1)*num_batches, :, :, :]
+          y = Y[t*num_batches:(t+1)*num_batches]
+          model.train()  # put model to training mode
+          x = x.to(device=device, dtype=dtype)  # move to device, e.g. GPU
+          y = y.to(device=device, dtype=torch.long)
+        
+          scores = model(x)
+          y_one_hots = torch.zeros_like(scores)
+          y_one_hots[np.arange(y.size(dim=0)),y] = 1
+          # print('scores:', scores, 'y:' , y_one_hots)
+          loss = F.cross_entropy(scores, y_one_hots)
+
+          # Zero out all of the gradients for the variables which the optimizer
+          # will update.
+          optimizer.zero_grad()
+
+          # This is the backwards pass: compute the gradient of the loss with
+          # respect to each  parameter of the model.
+          loss.backward()
+
+          # Actually update the parameters of the model using the gradients
+          # computed by the backwards pass.
+          optimizer.step()
+
+          if t % print_every == 0:
+              print('Iteration %d, loss = %.4f' % (t, loss.item()))
+              check_accuracy_part34(X, Y, model, "val")
+              print()
+
+
+def flatten(x):
+    N = x.shape[0] # read in N, C, H, W
+    return x.view(N, -1)  # "flatten" the C * H * W values into a single vector per image
+
+# We need to wrap `flatten` function in a module in order to stack it
+# in nn.Sequential
+class Flatten(nn.Module):
+    def forward(self, x):
+        return flatten(x)
+
+
+best_val = 0
+best_lr = None
+
+model = None
+optimizer = None
+
+channel_0 = 8
+channel_1 = 64
+channel_2 = 64
+channel_3 = 32
+hidden_layer_size_1 = 128
+hidden_layer_size_2 = 32
+learning_rates = [1e-3, 1e-4, 1e-5, 1e-6, 1e-7]
+drop_probs = [0, 0.2, 0.5, 0.8]
+
+for drop_prob in drop_probs:
+    for learning_rate in learning_rates:
+	#TODO: WRONG PADDING ("same" padding does not work for PyTorch versions under 1.9.0)
+        model = nn.Sequential(
+        nn.Conv2d(channel_0, channel_1, (3, 3), padding=(1,1)),
+        nn.ReLU(),
+        nn.MaxPool2d((2, 2), stride=2),  # changes H, W from 32 to 16
+        nn.Dropout2d(drop_prob),
+        nn.Conv2d(channel_1, channel_2, (3, 3), padding=(1,1)),
+        nn.ReLU(),
+        nn.MaxPool2d((2, 2), stride=2),  # changes H, W from 16 to 8
+        nn.Dropout2d(drop_prob),
+        nn.Conv2d(channel_2, channel_3, (3, 3), padding=(1,1)),
+        nn.ReLU(),
+        nn.MaxPool2d((2, 2), stride=2),  # changes H, W from 8 to 4
+        nn.BatchNorm2d(num_features = channel_3),
+        Flatten(),
+        nn.Linear(15376, hidden_layer_size_1),
+        nn.ReLU(),
+        nn.Dropout(drop_prob),
+        nn.Linear(hidden_layer_size_1, hidden_layer_size_2),    
+        nn.ReLU(),
+        nn.Linear(hidden_layer_size_2, 167),
+        )
+        optimizer = optim.SGD(model.parameters(), lr=learning_rate, momentum=0.9, nesterov=True)
+
+        print('LEARNING RATE:', learning_rate, 'DROP PROB:', drop_prob)
+    train_part34(model, optimizer, epochs=5, val_or_test="val")
+    val_acc, r2 = check_accuracy_part34(val_X, val_Y, model, "val")
+    if r2 > best_val:
+        best_model = model
+        best_lr = learning_rate
+
+check_accuracy_part34(test_X, test_Y, best_model, "test")
---
+++ Colab/EC2 Code/data_preprocess_ec2.py
@@ -0,0 +1,134 @@
+# Preprocessing Data for EC2 Instance
+
+# This code is used to preprocess the data on the EC2 Instance.
+# You should only have to run it **ONCE**, the cleaned data should be saved to this folder on the instance:
+# [insert folder].
+
+import sys
+from concurrent.futures import ThreadPoolExecutor
+import os
+import magic
+
+import numpy as np
+import pandas as pd
+import sklearn
+from tqdm.auto import tqdm
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader
+from torch.utils.data import sampler
+
+import torchvision.datasets as dset
+import torchvision.transforms as T
+
+import torch.nn.functional as F  # useful stateless functions
+
+# dhs_final_labels.csv file in 231nproj
+df = pd.read_csv('../../../../231nproj/dhs_final_labels.csv')
+df['survey'] = df['DHSID_EA'].str[:10]
+df['cc'] = df['DHSID_EA'].str[:2]
+
+data_dir = '../../../../231nproj/data/'
+df['path'] = data_dir + df['survey'] + '/' + df['DHSID_EA'] + '.npz'
+
+path_years = df[['DHSID_EA', 'path', 'year']].apply(tuple, axis=1)
+df.set_index('DHSID_EA', verify_integrity=True, inplace=True, drop=False) #had to add drop=False to keep column from disappearing  -- R
+print(df['path'].iloc[0])
+df.info()
+
+def paths_to_X(paths):  # -> (N, C, H, W) model input X
+  '''
+    Args
+    - paths: array (N, 1)
+      - path: str, path to npz file containing single entry 'x'
+        representing a (C, H, W) image
+
+    Returns: X, input matrix (N, C, H, W)
+    '''
+  N = len(paths)  # should be 117644
+  C, H, W = 8, 255, 255
+
+  imgs = []
+  for n in range(N):
+    npz_path = paths[n][0]
+    imgs.append(np.load(npz_path)['x'])  # shape (C, H, W)
+    if n % 2000  == 0:
+        print('On example', n)
+
+  return np.stack(imgs, axis=0)
+
+label = "n_under5_mort"
+
+SPLITS = {
+    'train': [
+        'AL', 'BD', 'CD', 'CM', 'GH', 'GU', 'HN', 'IA', 'ID', 'JO', 'KE', 'KM',
+        'LB', 'LS', 'MA', 'MB', 'MD', 'MM', 'MW', 'MZ', 'NG', 'NI', 'PE', 'PH',
+        'SN', 'TG', 'TJ', 'UG', 'ZM', 'ZW'],
+    'val': [
+        'BF', 'BJ', 'BO', 'CO', 'DR', 'GA', 'GN', 'GY', 'HT', 'NM', 'SL', 'TD',
+        'TZ'],
+    'test': [
+        'AM', 'AO', 'BU', 'CI', 'EG', 'ET', 'KH', 'KY', 'ML', 'NP', 'PK', 'RW',
+        'SZ']
+}
+
+SPLITS['trainval'] = SPLITS['train'] + SPLITS['val']
+
+#partial splits
+
+SPLITS['train_partial'] = SPLITS['train'][:1] #used to be:5
+SPLITS['val_partial'] = SPLITS['val'][:1] #used to be :2
+SPLITS['test_partial'] = SPLITS['test'][:1]	#used to be :2
+SPLITS['trainval_partial'] = SPLITS['train_partial'] + SPLITS['val_partial']
+
+
+def get_data_split(label, split):
+    train_dhsids = df.index[df['cc'].isin(SPLITS[split]) & df[label].notna()]
+
+    train_X_paths = df.loc[train_dhsids, 'path'].values.reshape(-1, 1)
+    train_X = paths_to_X(train_X_paths)
+    train_Y = df.loc[train_dhsids, label].values
+
+    # knn.fit(train_X, train_Y)
+    # preds = knn.predict(test_X)
+    return train_X, train_Y
+
+data_dir = '../../../../231nproj/'
+train_X, train_Y = get_data_split(label, 'train_partial')
+print("train_X: ", train_X.shape)
+print("train_Y: ", train_Y.shape)
+print('Saving data in folder' + str(data_dir) + 'data_clean/train_partial')
+np.savez_compressed(str(data_dir) + 'data_clean/train_partial', train_X=train_X, train_Y=train_Y)
+
+val_X, val_Y = get_data_split(label, 'val_partial')
+print("val_X: ", val_X.shape)
+print("val_Y: ", val_Y.shape)
+print('Saving data in folder' + str(data_dir) + 'data_clean/val_partial')
+np.savez_compressed(str(data_dir) + 'data_clean/val_partial', val_X=val_X, val_Y=val_Y)
+
+test_X, test_Y = get_data_split(label, 'test_partial')
+print("test_X: ", test_X.shape)
+print("test_Y: ", test_Y.shape)
+print('Saving data in folder' + str(data_dir) + 'data_clean/test_partial')
+np.savez_compressed(str(data_dir) + 'data_clean/test_partial', test_X=test_X, test_Y=test_Y)
+
+
+# train_X, train_Y = get_data_split(label, 'train')
+# print("train_X: ", train_X.shape)
+# print("train_Y: ", train_Y.shape)
+# print('Saving data in folder' + str(data_dir) + '/data_clean/train')
+# np.savez_compressed(str(data_dir) + 'data_clean/train', train_X=train_X, train_Y=train_Y)
+
+# val_X, val_Y = get_data_split(label, 'val')
+# print("val_X: ", val_X.shape)
+# print("val_Y: ", val_Y.shape)
+# print('Saving data in folder' + str(data_dir) + '/data_clean/val')
+# np.savez_compressed(str(data_dir) + 'data_clean/val', val_X=val_X, val_Y=val_Y)
+
+# test_X, test_Y = get_data_split(label, 'test')
+# print("test_X: ", test_X.shape)
+# print("test_Y: ", test_Y.shape)
+# print('Saving data in folder' + str(data_dir) + '/data_clean/test')
+# np.savez_compressed(str(data_dir) + 'data_clean/test', test_X=test_X, test_Y=test_Y)
